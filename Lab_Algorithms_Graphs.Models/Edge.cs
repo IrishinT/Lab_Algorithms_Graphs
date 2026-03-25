466b90f43b5117df3aec0f0df18f10ffdab21132
Тимофей <t.irishin@inbox.ru>
2026-03-25 13:15:30 +0300

    Добавление модели данных Vertex и Edge
    - Vertex.cs: класс вершины графа (Студент).
      Свойства: Id (уникальный ключ), Label (отображаемое имя).
      Переопределены Equals/GetHashCode для корректного сравнения в коллекциях.
    - Edge.cs: класс ребра (Знакомство).
      Свойства: From, To (вершины), Weight (зарезервировано для ЛР №5).
      Поддержка неориентированности (сравнение ребер A-B и B-A как равных).

---
+++ Lab_Algorithms_Graphs.Models/Edge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Algorithms_Graphs.Models
+{
+    public class Edge
+    {
+        public Vertex From { get; }
+        public Vertex To { get; }
+        public double Weight { get; }  // Для ЛР №4 пока не используется, но закладываем на будущее
+
+        public Edge(Vertex from, Vertex to, double weight = 1.0)
+        {
+            From = from;
+            To = to;
+            Weight = weight;
+        }
+
+        // Граф неориентированный: ребро A-B эквивалентно B-A
+        public override bool Equals(object? obj) =>
+            obj is Edge e &&
+            ((From.Equals(e.From) && To.Equals(e.To)) ||
+             (From.Equals(e.To) && To.Equals(e.From)));
+
+        public override int GetHashCode() =>
+            From.GetHashCode() ^ To.GetHashCode();
+    }
+}
---
+++ Lab_Algorithms_Graphs.Models/Vertex.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab_Algorithms_Graphs.Models
+{
+    public class Vertex
+    {
+        public string Id { get; }      // Уникальный идентификатор (имя студента)
+        public string Label { get; }   // Отображаемое имя
+
+        public Vertex(string id, string? label = null)
+        {
+            Id = id;
+            Label = label ?? id;
+        }
+
+        public override string ToString() => Label;
+        public override bool Equals(object? obj) => obj is Vertex v && Id == v.Id;
+        public override int GetHashCode() => Id.GetHashCode();
+    }
+}
